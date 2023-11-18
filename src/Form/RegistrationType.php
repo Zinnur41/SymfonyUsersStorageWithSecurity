49cb8ef41da2d2490f0c8a52a99c6e74dd92fb4f
Zinnur41 <zagidullin_zin@mail.ru>
2023-11-18 21:52:06 +0300

    add constraints

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -9,15 +9,27 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('secondName', TextType::class)
-            ->add('thirdName', TextType::class)
+            ->add('firstName', TextType::class, [
+                'constraints' => [new NotBlank(),
+                    new Regex('[a-zA-Z]', message: 'В имени не должно быть цифр!')]
+            ])
+            ->add('secondName', TextType::class, [
+                'constraints' => [new NotBlank(),
+                    new Regex('[a-zA-Z]', message: 'В фамилии не должно быть цифр!')]
+            ])
+            ->add('thirdName', TextType::class, [
+                'constraints' => [new NotBlank(),
+                    new Regex('[a-zA-Z]', message: 'В отчестве не должно быть цифр!')]
+            ])
             ->add('age', IntegerType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
